039f81410142ee8693ac1402421c7b3c46f16c57
zahra80krm <117530012+zahra80krm@users.noreply.github.com>
2022-11-13 08:15:50 -0800

    Add files via upload

---
+++ p52.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int char;
+    char hello;
+    cout <<"please enter word :";
+    cin >>char;
+    
+    switch (hello){
+
+        case 'o':
+            cout << "vowels";
+            break;
+        case 'a':
+            cout << "vowels";
+            break;
+        case 'e':
+        	cout << "vowels";
+        	break;
+        case 'i':
+        	cout << "vowels";
+        	break;
+        case 'u':
+        	cout << "vowels";
+        	
+}
