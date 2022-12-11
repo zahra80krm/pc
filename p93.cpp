70605737508ad0bc16ebb54690ffe72d3d4d831d
zahra80krm <117530012+zahra80krm@users.noreply.github.com>
2022-12-11 11:17:55 -0800

    Add files via upload

---
+++ p93.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace  std;
+
+int main() {
+	
+	int a[10];
+	int i;
+	
+	a[0]= 1;
+	a[1]= 1;
+	
+	for(i=2; i<10 ;i++)
+	       a[i] = a[i-1]+ a[i-2;]
+	       
+	       
+	        for(i=0;i<10;i++)
+	                cout<<a[i]<<"  ";
+	                
+	                
+}
